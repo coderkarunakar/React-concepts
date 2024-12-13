1261f3506daa37f28c2fbc82e3a84d2cac14c33e
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-12-13 06:42:39 +0530

    working with usestate and making to show age

--- reactlearning/src/ContactCard.js
+++ reactlearning/src/ContactCard.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 // it is a good idea to call component name as our react filename
 const ContactCard =()=> {
+  const [showAge,setShowAge] = useState(false);
     return(
     <div className="contact-card">
     <img src="https://via.placeholder.com/150" alt="profile">
     </img>
     <div className="user-details">
+      {/* hadcoding name email and age */}
       <p>Name: Jenny hanna</p>
       <p>Email: jenny.hanna@notreal.com</p>
-      <p>Age: 25</p>
+      <button onClick={()=>setShowAge(!showAge)}>showAge</button>
+      {showAge ? <p>Age: 25</p> : null}
     </div>
 
   </div>
