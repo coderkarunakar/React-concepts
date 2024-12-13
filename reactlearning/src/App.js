6c2d0385124d32ed6e7308124a3032d0546ea120
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-12-13 08:39:40 +0530

    receiving data from real  api using usestate and fetch

--- reactlearning/src/App.js
+++ reactlearning/src/App.js
@@ -1,72 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ContactCard from './ContactCard';
 import './App.css';
 //simple arrow function we used to declare a component, this component break down the  UI into smaller , reusable peices, making it easier to manage and develop complex application
 const App = () =>{
-  const contacts = [
-    {
-      avatarUrl : "https://via.placeholder.com/150",
-      name :"Jenny han",
-      email :"jenny.han@notreal.com",
-      age : 25
-    },
-    {
-      avatarUrl : "https://via.placeholder.com/150",
-      name :"pual",
-      email :"paul.han@pnotreal.com",
-      age : 22
-    },
-    {
-      avatarUrl : "https://via.placeholder.com/150",
-      name :"kaira han",
-      email :"kaira.han@notreal.com",
-      age : 28
-    },
-    {
-      avatarUrl : "https://via.placeholder.com/150",
-      name :"siru",
-      email :"kaira.han@notreal.com",
-      age : 34
-    },
-  ]
+  // const contacts = [
+  //   {
+  //     avatarUrl : "https://via.placeholder.com/150",
+  //     name :"Jenny han",
+  //     email :"jenny.han@notreal.com",
+  //     age : 25
+  //   },
+  //   {
+  //     avatarUrl : "https://via.placeholder.com/150",
+  //     name :"pual",
+  //     email :"paul.han@pnotreal.com",
+  //     age : 22
+  //   },
+  //   {
+  //     avatarUrl : "https://via.placeholder.com/150",
+  //     name :"kaira han",
+  //     email :"kaira.han@notreal.com",
+  //     age : 28
+  //   },
+  //   {
+  //     avatarUrl : "https://via.placeholder.com/150",
+  //     name :"siru",
+  //     email :"kaira.han@notreal.com",
+  //     age : 34
+  //   },
+  // ]
+
+  const [results, setResults] = useState([]);
+  // here we are fetching data from backend json converts to json  and trying to console log the response data
+  fetch("https://randomuser.me/api/?results=5")
+  .then(response => response.json())
+  .then(data => {console.log(data)
+  setResults(data.results)
+  }
+  ); 
+
    return(
     <div>
       {/* //a map has an object(arrayname where it needs to be iterated) ,and its postion */}
-      {contacts.map((  person, index)=>{
+      {results.map((  result, index)=>{
         return (
           <ContactCard key = {index}
   
-          avatarUrl = {person.avatarUrl}
-          name = {person.name}
-          email = {person.email}
-          age ={person.age}
+          avatarUrl = {result.picture.large}
+          name = {result.name.first}
+          email = {result.email}
+          age ={result.dob.age}
           />
 
         )
 
       })}
-      {/* <ContactCard 
-      // this below are called props and if it is a string need to put inside a string other than a string need to put inside a {}
-      avatarUrl = "https://via.placeholder.com/150"
-      name ="Jenny han"
-      email ="jenny.han@notreal.com"
-      age ={25}
-      /> */}
-      {/* <ContactCard
-      avatarUrl = "https://via.placeholder.com/150"
-      name ="Jenny han"
-      email ="jenny.han@notreal.com"
-      age ={25}/>
-      <ContactCard
-      avatarUrl = "https://via.placeholder.com/150"
-      name ="jesy"
-      email ="jesy.han@notreal.com"
-      age ={25}/>
-      <ContactCard
-      avatarUrl = "https://via.placeholder.com/150"
-      name ="peter"
-      email ="peter.han@notreal.com"
-      age ={25}/> */}
+      
     </div>
  
    )
